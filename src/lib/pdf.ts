0ebe63f06366d32ee73cddf6bae826e6f0a51e53
thedadreport <thedadreportemail@gmail.com>
2025-06-11 14:47:23 -0400

    Fix: Major refactor of PDF module exports to fix TypeScript errors

--- src/lib/pdf.ts
+++ src/lib/pdf.ts
@@ -1,2 +1,21 @@
 // Re-export everything from the PDF module
-export * from './pdf/index';
+import { styles, colors } from './pdf/styles';
+import RecipeCardPDF from './pdf/RecipeCard';
+import MealPlanPDF from './pdf/MealPlan';
+import { 
+  generateRecipePDF, 
+  generateMealPlanPDF,
+  cleanupOldPDFs,
+  getUserPDFs 
+} from './pdf/generator';
+
+export {
+  styles,
+  colors,
+  RecipeCardPDF,
+  MealPlanPDF,
+  generateRecipePDF,
+  generateMealPlanPDF,
+  cleanupOldPDFs,
+  getUserPDFs
+};
